e8920d2c87c6f4a38c781fffca0bb523b6afbbf2
Sanket Banate <sanketbanate@Sankets-MacBook-Air.local>
2025-11-27 01:15:12 +0530

    Fix NODE_OPTIONS issue - use Node 18

---
+++ frontend/build/precache-manifest.c12d3f81df11c2fbea08c88bf4412d12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efb7bee23082820f14625bd8823a0134",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27a50e1f1e5adb018a66",
+    "url": "/static/css/main.9b6f95f1.chunk.css"
+  },
+  {
+    "revision": "13fd676da27519fdf826",
+    "url": "/static/js/2.b65ec397.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/static/js/2.b65ec397.chunk.js.LICENSE"
+  },
+  {
+    "revision": "27a50e1f1e5adb018a66",
+    "url": "/static/js/main.b64aabb4.chunk.js"
+  },
+  {
+    "revision": "05e2d2637e83c42dc136",
+    "url": "/static/js/runtime-main.4ee28378.js"
+  }
+]);
